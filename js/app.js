802cec0c90ba95eff94eab60788c94d8cc327a28
lvisentin <lvise.batista@gmail.com>
2021-02-15 16:33:23 -0300

    removing loader

--- js/app.js
+++ js/app.js
@@ -1,11 +1,11 @@
 let timeLine = new TimelineMax();
 
 document.addEventListener('DOMContentLoaded', () => {
-    document.querySelector('body').style.overflow = 'hidden';
+    // document.querySelector('body').style.overflow = 'hidden';
     timeLine
-        .fromTo('.bg-loader', 1,
-            { width: '100%' },
-            { width: '0%', delay: 3, ease: Expo.easeInOut })
+        // .fromTo('.bg-loader', 1,
+        //     { width: '100%' },
+        //     { width: '0%', delay: 3, ease: Expo.easeInOut })
         .fromTo('.profile-img', 1,
             { y: -100, opacity: 0 },
             { y: 0, opacity: 1, ease: Expo.easeInOut }, '-=0.7')
@@ -14,16 +14,19 @@ document.addEventListener('DOMContentLoaded', () => {
             { y: 0, opacity: 1, ease: Expo.easeInOut }, '-=0.7')
         .fromTo('.social-links', 1,
             { y: -50, opacity: 0 },
-            { y: 0, opacity: 1, ease: Expo.easeInOut }, '-=0.7');
+            { y: 0, opacity: 1, ease: Expo.easeInOut }, '-=0.7')
+            .fromTo('.posts-section', 1,
+    { y: -50, opacity: 0 },
+    { y: 0, opacity: 1, ease: Expo.easeInOut }, '-=0.7');
 });
 
 
-setTimeout(async () => {
-    // document.querySelector('.posts-section').style.display = 'block';
+// setTimeout(async () => {
+//     // document.querySelector('.posts-section').style.display = 'block';
     
-    await timeLine.fromTo('.posts-section', 1,
-    { y: -50, opacity: 0 },
-    { y: 0, opacity: 1, ease: Expo.easeInOut }, '-=0.7')
+//     await timeLine.fromTo('.posts-section', 1,
+//     { y: -50, opacity: 0 },
+//     { y: 0, opacity: 1, ease: Expo.easeInOut }, '-=0.7')
     
-    document.querySelector('body').style.overflow = 'auto';
-}, 3000);
+//     // document.querySelector('body').style.overflow = 'auto';
+// }, 3000);
