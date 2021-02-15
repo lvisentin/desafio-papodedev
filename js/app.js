947ed36e956146b471b2b1d59fd1ff1e2c1ed7a8
lvisentin <lvise.batista@gmail.com>
2021-02-15 16:21:45 -0300

    css fix

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 let timeLine = new TimelineMax();
 
 document.addEventListener('DOMContentLoaded', () => {
+    document.querySelector('body').style.overflow = 'hidden';
     timeLine
         .fromTo('.bg-loader', 1,
             { width: '100%' },
@@ -18,7 +19,11 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 setTimeout(async () => {
+    // document.querySelector('.posts-section').style.display = 'block';
+    
     await timeLine.fromTo('.posts-section', 1,
     { y: -50, opacity: 0 },
     { y: 0, opacity: 1, ease: Expo.easeInOut }, '-=0.7')
+    
+    document.querySelector('body').style.overflow = 'auto';
 }, 3000);
