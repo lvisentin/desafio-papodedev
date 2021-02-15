7f07ac4810ebc09ccbeb4cba66736dbbb9b769ac
lvisentin <lvise.batista@gmail.com>
2021-02-14 23:31:41 -0300

    test correction post section

--- js/app.js
+++ js/app.js
@@ -6,9 +6,9 @@ document.addEventListener('DOMContentLoaded', () => {
         .fromTo('.bg-loader', 1,
             { width: '100%' },
             { width: '0%', delay: 3, ease: Expo.easeInOut })
-        .fromTo('.posts-section', 1,
-            { height: '0%' },
-            { height: '100%', ease: Expo.easeInOut }, '-=0.7')
+        // .fromTo('.posts-section', 1,
+        //     { y: -100, opacity: 0 },
+        //     { y: 0, opacity: 1, ease: Expo.easeInOut }, '-=0.7')
         .fromTo('.profile-img', 1,
             { y: -100, opacity: 0 },
             { y: 0, opacity: 1, ease: Expo.easeInOut }, '-=0.7')
